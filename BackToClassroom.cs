d437af02670d49bf206fdb1933e429e5491bb437
marksteelz3 <31002690+marksteelz3@users.noreply.github.com>
2018-10-14 13:31:00 -0400

    Push

---
+++ BackToClassroom.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToClassroom : MonoBehaviour {
+
+	public AudioSource aud;
+	public float delay;
+	
+	// Update is called once per frame
+	void Update () {
+		if (!aud.isPlaying && Time.time > delay){
+            SceneManager.LoadSceneAsync("Classroom");
+		}
+	}
+}
---
+++ GrabSceneLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using VRTK;
+
+public class GrabSceneLoader : MonoBehaviour {
+	
+	public string sceneToLoad;
+	public VRTK_InteractableObject grabber;
+	private bool hasBeenGrabbed;
+
+
+	void Start(){
+		hasBeenGrabbed = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (grabber.IsGrabbed() && !hasBeenGrabbed){
+			LoadScene();
+			hasBeenGrabbed = true;
+		}
+	}
+
+	void LoadScene(){
+		SceneManager.LoadSceneAsync(sceneToLoad);
+	}
+}
